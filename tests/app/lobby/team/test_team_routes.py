9adb1aef8b81db33856278a3766745ca6459de04
Arios Jentu <darthpoezd@gmail.com>
2025-03-05 03:52:28 +0400

    Update Teams Routes test to cover all available exceptions

--- tests/app/lobby/team/test_team_routes.py
+++ tests/app/lobby/team/test_team_routes.py
@@ -56,13 +56,29 @@ async def test_teams_routes_require_auth(
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "team_data, expected_status, error_substr", 
+    "team_data, expected_status_team, error_substr_team",
     [
-        ({"name": "New Team"},  200,    ""),
-        ({"name": "   "},       422,    "name cannot be empty"),
+        ({"name":   "New Team"},    200,    ""),
+        ({"name":   "   "},         422,    "name cannot be empty"),
+        ({},                        422,    "Field required"),
     ]
 )
-@pytest.mark.parametrize("role", Roles.LIST)
+@pytest.mark.parametrize(
+    "role, is_lobby_owner, expected_status_access, error_substr_access",
+    [
+        (UserRole.ADMIN,        False,  200,    ""),
+        (UserRole.MODERATOR,    False,  200,    ""),
+        (UserRole.USER,         True,   200,    ""),
+        (UserRole.USER,         False,  403,    "No access to control team"),
+    ]
+)
+@pytest.mark.parametrize(
+    "lobby_exists, expected_status_lobby, error_substr_lobby",
+    [
+        (True,  200,    ""),
+        (False, 404,    "Lobby not found"),
+    ]
+)
 async def test_create_team(
         client_async: AsyncClient,
         user_factory: UserFactory,
@@ -70,92 +86,114 @@ async def test_create_team(
         test_algorithm: Algorithm,
         lobby_factory: LobbyFactory,
         team_data: dict[str, str],
-        expected_status: int,
-        error_substr: str,
-        role: UserRole
-):
-    route = "/api/v1/teams/"
-    owner, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
-    headers = {"Authorization": f"Bearer {access_token}"}
-    
-    lobby = await lobby_factory.create(owner, test_algorithm)
-    team_data["lobby_id"] = lobby.id
-
-    response: Response = await client_async.post(route, json=team_data, headers=headers)
-    assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
-
-    json_data = response.json()
-    if expected_status == 200:
-        assert json_data["name"] == team_data["name"], "Team name does not match"
-    else:
-        assert error_substr in json_data["detail"][0]["msg"], f"Message doesn't contains substring: {json_data["detail"][0]["msg"]}"
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("team_data", [{"name": "Unauthorized Team"}])
-@pytest.mark.parametrize("role, expected_status", [
-    (UserRole.USER,         403),
-    (UserRole.MODERATOR,    200),
-    (UserRole.ADMIN,        200),
-])
-async def test_create_team_in_lobby(
-        client_async: AsyncClient,
-        user_factory: UserFactory,
-        token_factory: TokenFactory,
-        lobby_factory: LobbyFactory,
-        test_algorithm: Algorithm,
-        team_data: dict[str, str],
+        expected_status_team: int,
+        error_substr_team: str,
         role: UserRole,
-        expected_status: int
+        is_lobby_owner: bool,
+        expected_status_access: int,
+        error_substr_access: str,
+        lobby_exists: bool,
+        expected_status_lobby: int,
+        error_substr_lobby: str
 ):
 
     route = "/api/v1/teams/"
-    _, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
+    
+    user, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
     creator, _, _ = await create_user_with_tokens(user_factory, token_factory, prefix="creator_user")
     headers = {"Authorization": f"Bearer {access_token}"}
 
-    lobby = await lobby_factory.create(creator, test_algorithm)
-    team_data["lobby_id"] = lobby.id
+    if not lobby_exists:
+        team_data["lobby_id"] = -1 
+    else:
+        if is_lobby_owner:
+            lobby = await lobby_factory.create(user, test_algorithm)
+        else:
+            lobby = await lobby_factory.create(creator, test_algorithm)
+
+        team_data["lobby_id"] = lobby.id
 
     response: Response = await client_async.post(route, json=team_data, headers=headers)
-    assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
+    json_data = response.json()
 
-    if expected_status == 200:
-        json_data = response.json()
-        assert json_data["name"] == team_data["name"]
-        assert json_data["lobby"]["id"] == lobby.id
+    if expected_status_team != 200:
+        assert response.status_code == expected_status_team, f"Expected {expected_status_team}, got {response.status_code}"
+        assert error_substr_team in json_data["detail"][0]["msg"], f"Validation error mismatch: {json_data['detail'][0]['msg']}"
+        return
+
+    if expected_status_lobby != 200:
+        assert response.status_code == expected_status_lobby, f"Expected {expected_status_lobby}, got {response.status_code}"
+        assert error_substr_lobby in json_data["detail"], f"Expected error message '{error_substr_lobby}', got: {json_data['detail']}"
+        return
+
+    assert response.status_code == expected_status_access, f"Expected {expected_status_access}, got {response.status_code}"
+    if expected_status_access != 200:
+        assert error_substr_access in json_data["detail"], f"Expected error message '{error_substr_access}', got: {json_data['detail']}"
+        return
+
+    assert json_data["name"] == team_data["name"], "Team name does not match"
+    assert json_data["lobby"]["id"] == team_data["lobby_id"], "Lobby ID does not match"
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("role", Roles.LIST)
+@pytest.mark.parametrize(
+    "team_exists, expected_status, error_msg",
+    [
+        (True,  200, ""),
+        (False, 404, "Team not found"),
+    ]
+)
 async def test_get_team_info(
         client_async: AsyncClient,
         user_factory: UserFactory,
         token_factory: TokenFactory,
         test_team_id: int,
-        role: UserRole
+        role: UserRole,
+        team_exists: bool,
+        expected_status: int,
+        error_msg: str
 ):
 
-    route = f"/api/v1/teams/{test_team_id}"
+    team_id = test_team_id if team_exists else -1
+    route = f"/api/v1/teams/{team_id}"
+
     _, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
     headers = {"Authorization": f"Bearer {access_token}"}
 
     response: Response = await client_async.get(route, headers=headers)
-    assert response.status_code == 200, f"Expected 200, got {response.status_code}"
-
     json_data = response.json()
+
+    assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
+    if not team_exists:
+        assert error_msg in json_data["detail"], f"Expected error message '{error_msg}', got: {json_data['detail']}"
+        return 
+    
     assert json_data["id"] == test_team_id, "Team ID does not match"
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("update_data", [{"name": "Updated Team Name"}])
 @pytest.mark.parametrize(
-    "role, is_lobby_owner, expected_status",
+    "update_data, expected_status, error_msg",
+    [
+        ({"name":   "Updated Team Name"},   200,    ""),
+        ({"name":   "  "},                  422,    "name cannot be empty"),
+    ]
+)
+@pytest.mark.parametrize(
+    "role, is_lobby_owner, expected_status_access",
     [
         (UserRole.ADMIN,        False,  200),
         (UserRole.MODERATOR,    False,  200),
         (UserRole.USER,         True,   200),
         (UserRole.USER,         False,  403),
+    ]
+)
+@pytest.mark.parametrize(
+    "team_exists, expected_status_exists, error_msg_exists",
+    [
+        (True,  200,    ""),
+        (False, 404,    "Team not found"),
     ]
 )
 async def test_update_team(
@@ -168,32 +206,52 @@ async def test_update_team(
         update_data: dict[str, str],
         role: UserRole,
         is_lobby_owner: bool,
-        expected_status: int
+        team_exists: bool,
+        expected_status: int,
+        expected_status_access: int,
+        expected_status_exists: int,
+        error_msg: str,
+        error_msg_exists: str,
 ):
-    
-    owner, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
-    other_owner, _, _ = await create_user_with_tokens(user_factory, token_factory, prefix="creator_user")
+
+    user, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
+    creator, _, _ = await create_user_with_tokens(user_factory, token_factory, prefix="creator_user")
     headers = {"Authorization": f"Bearer {access_token}"}
 
     if is_lobby_owner:
-        lobby = await lobby_factory.create(owner, test_algorithm)
+        lobby = await lobby_factory.create(user, test_algorithm)
     else:
-        lobby = await lobby_factory.create(other_owner, test_algorithm)
+        lobby = await lobby_factory.create(creator, test_algorithm)
 
-    team = await team_factory.create(lobby)
-    route = f"/api/v1/teams/{team.id}"
+    team_id = -1
+    if team_exists:
+        team = await team_factory.create(lobby)
+        team_id = team.id
+
+    route = f"/api/v1/teams/{team_id}"
 
     response: Response = await client_async.put(route, json=update_data, headers=headers)
-    assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
+    if expected_status != 200:
+        assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
+        assert error_msg in response.json()["detail"][0]["msg"], f"Expected validation error message '{error_msg}', got: {response.json()['detail'][0]['msg']}"
+        return
 
-    if expected_status == 200:
-        json_data = response.json()
-        assert json_data["name"] == update_data["name"], "Team name was not updated"
+    if expected_status_exists != 200:
+        assert response.status_code == expected_status_exists, f"Expected {expected_status_exists}, got {response.status_code}"
+        assert error_msg_exists in response.json()["detail"], f"Expected error message '{error_msg_exists}', got: {response.json()['detail']}"
+        return
+
+    assert response.status_code == expected_status_access, f"Expected {expected_status_access}, got {response.status_code}"
+    if expected_status_access != 200:
+        return
+
+    json_data = response.json()
+    assert json_data["name"] == update_data["name"], "Team name was not updated"
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "role, is_lobby_owner, expected_status",
+    "role, is_lobby_owner, expected_status_access",
     [
         (UserRole.ADMIN,        False,  200),
         (UserRole.MODERATOR,    False,  200),
@@ -201,7 +259,14 @@ async def test_update_team(
         (UserRole.USER,         False,  403),
     ]
 )
-async def test_delete_team_different_users(
+@pytest.mark.parametrize(
+    "team_exists, expected_status_exists, error_msg_exists",
+    [
+        (True,  200,    ""),
+        (False, 404,    "Team not found"),
+    ]
+)
+async def test_delete_team(
         client_async: AsyncClient,
         user_factory: UserFactory,
         token_factory: TokenFactory,
@@ -210,44 +275,42 @@ async def test_delete_team_different_users(
         test_algorithm: Algorithm,
         role: UserRole,
         is_lobby_owner: bool,
-        expected_status: int
+        team_exists: bool,
+        expected_status_access: int,
+        expected_status_exists: int,
+        error_msg_exists: str,
 ):
 
-    owner, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
-    other_owner, _, _ = await create_user_with_tokens(user_factory, token_factory, prefix="creator_user")
+    user, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
+    creator, _, _ = await create_user_with_tokens(user_factory, token_factory, prefix="creator_user")
     headers = {"Authorization": f"Bearer {access_token}"}
 
     if is_lobby_owner:
-        lobby = await lobby_factory.create(owner, test_algorithm)
+        lobby = await lobby_factory.create(user, test_algorithm)
     else:
-        lobby = await lobby_factory.create(other_owner, test_algorithm)
+        lobby = await lobby_factory.create(creator, test_algorithm)
 
-    team = await team_factory.create(lobby)
-    route = f"/api/v1/teams/{team.id}"
+    team_id = -1
+    if team_exists:
+        team = await team_factory.create(lobby)
+        team_id = team.id
 
-    response: Response = await client_async.delete(route, headers=headers)
-    assert response.status_code == expected_status, f"Expected {expected_status}, got {response.status_code}"
-
-    if expected_status == 200:
-        json_data = response.json()
-        assert json_data["description"] == f"Team '{team.name}' deleted successfully"
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("role", Roles.LIST)
-async def test_delete_team_not_found(
-        client_async: AsyncClient,
-        user_factory: UserFactory,
-        token_factory: TokenFactory,
-        role: UserRole
-):
+    route = f"/api/v1/teams/{team_id}"
     
-    route = f"/api/v1/teams/1"
-    _, access_token, _ = await create_user_with_tokens(user_factory, token_factory, role)
-    headers = {"Authorization": f"Bearer {access_token}"}
-
     response: Response = await client_async.delete(route, headers=headers)
-    assert response.status_code == 404, f"Expected 404, got {response.status_code}"
+    if expected_status_exists != 200:
+        assert response.status_code == expected_status_exists, f"Expected {expected_status_exists}, got {response.status_code}"
+        assert error_msg_exists in response.json()["detail"], f"Expected error message '{error_msg_exists}', got: {response.json()['detail']}"
+        return
+
+    if expected_status_access != 200:
+        assert response.status_code == expected_status_access, f"Expected {expected_status_access}, got {response.status_code}"
+        return
+
+    assert response.status_code == 200, f"Expected 200, got {response.status_code}"
+    
+    json_data = response.json()
+    assert json_data["description"] == f"Team '{team.name}' deleted successfully"
 
 
 filter_data = [
