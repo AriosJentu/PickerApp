239987bc5edc4f5315fe9dba65a069312ed3e4b5
Arios Jentu <darthpoezd@gmail.com>
2025-03-12 02:34:41 +0400

    Change error code for User Exception

--- app/exceptions/user.py
+++ app/exceptions/user.py
@@ -34,7 +34,7 @@ class HTTPUserExceptionNotFound(HTTPUserException):
 class HTTPUserExceptionNoDataProvided(HTTPUserException):
     def __init__(self, detail: str):
         super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=detail
         )
 
